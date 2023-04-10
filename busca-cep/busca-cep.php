c88de4bf1f97c54c50939f666cd169f44786d699
Amauri Junior <contato@amaurijunior.com>
2023-04-10 19:32:20 +0100

    adding the new brazilian postcode searcher

---
+++ busca-cep/busca-cep.php
@@ -0,0 +1,40 @@
+<form method="post">
+	<input type="text" placeholder="CEP" name="cep"></br>
+	<input type="submit" name="submit" value="enviar">
+</form>
+
+<script src="https://use.fontawesome.com/8ee07e83e4.js"></script>
+<script src="js/jquery-3.6.0.min.js"></script>
+<script src="js/functions.js"></script>
+<script src="js/jquery.mask.js"></script>
+
+<?php
+
+if(isset($_POST['cep'])){
+	$cep = str_replace("-","",$_POST['cep']);
+
+	function get_address($cep){
+		$url = "https://viacep.com.br/ws/".$cep."/json";
+
+		$json = json_decode(file_get_contents($url));
+		return $json;
+	}
+
+	$address = get_address($cep);
+
+	foreach ($address as $chave => $valor) {
+	  if ($chave == 'logradouro'){
+	  	echo $valor." - ";
+	  }
+	  if($chave == 'bairro'){
+	  	echo $valor." - ";
+	  }
+	  if($chave == 'localidade'){
+	  	echo $valor." - ";
+	  }	  if($chave == 'uf'){
+	  	echo $valor;
+	  }
+	}
+}
+
+?>
---
+++ busca-cep/js/functions.js
@@ -0,0 +1,3 @@
+$(function(){
+	$('input[name=cep]').mask('99999-999')
+})
